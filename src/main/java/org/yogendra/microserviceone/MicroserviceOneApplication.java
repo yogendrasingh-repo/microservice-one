5a0313023355a34507b780a45f772718e726371e
Yogendra Pratap Singh <yogendra.c.singh@consultant.husqvarnagroup.com>
2024-11-21 23:16:35 +0530

    Draft commit

---
+++ src/main/java/org/yogendra/microserviceone/MicroserviceOneApplication.java
@@ -0,0 +1,13 @@
+package org.yogendra.microserviceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceOneApplication.class, args);
+    }
+
+}
